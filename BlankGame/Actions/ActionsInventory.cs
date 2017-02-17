b985d2044c26c9b5e6ac8b0c76c1b7422a594ad3
Christopher Upshir <christopherupshir@ccvonline.com>
2017-02-16 21:17:13 -0700

    Updated to make the game case insensitive. Redesigned Adding/Removing items

--- BlankGame/Actions/ActionsInventory.cs
+++ BlankGame/Actions/ActionsInventory.cs
@@ -31,72 +31,40 @@ namespace BlankGame
             Console.WriteLine();
         }
 
-        public static void LookAtItem(string item)
+        public static void DisplayItemStats(Item item)
         {
-            List<Item> validItems = Item.ValidItems();
-
-            IEnumerable<Item> checkItem = validItems.Where(p => p.Name == item);
-            if (!checkItem.Any())
-            {
-                Console.WriteLine("That item doesnt exist");
-            }
-            else
-            {
-                Console.WriteLine(item + " Details");
+                Console.WriteLine(item.Name + " Details");
                 for (int i = 0; i < 50; i++)
                 {
                     Console.Write("-");
                 }
                 Console.WriteLine();
-                foreach (var prop in checkItem)
-                {
-                    Console.WriteLine("       Level: " + prop.Level);
-                    Console.WriteLine("    Strength: " + prop.Strength);
-                    Console.WriteLine("     Stamina: " + prop.Stamina);
-                    Console.WriteLine("     Agility: " + prop.Agility);
-                    Console.WriteLine("Intelligence: " + prop.Intellegence);
-                }
-            }
-
+                Console.WriteLine("       Level: " + item.Level);
+                Console.WriteLine("    Strength: " + item.Strength);
+                Console.WriteLine("     Stamina: " + item.Stamina);
+                Console.WriteLine("     Agility: " + item.Agility);
+                Console.WriteLine("Intelligence: " + item.Intellegence);
+            
             Console.WriteLine();
         }
 
-        public static Tuple<Room, List<Item>> AddToInventory(Room currentRoom, string item, List<Item> inventory)
+        public static Tuple<Room, List<Item>> AddToInventory(Room currentRoom, Item item, List<Item> inventory)
         {
-
-            IEnumerable<Item> itemCheck = currentRoom.Inventory.Where(p => p.Name == item);
-                
-            if (!itemCheck.Any())
-            {
-                Console.WriteLine("That item does not exist here!");
-            }
-            else
-            {
-                Item newItem = itemCheck.Single();
-                inventory.Add(newItem);
-                currentRoom.Inventory.Remove(newItem);
-                Console.WriteLine();
-                Console.WriteLine(item + " has been added to your inventory.");
-            }
-
+            inventory.Add(item);
+            currentRoom.Inventory.Remove(item);
+            Console.WriteLine();
+            Console.WriteLine(item.Name + " has been added to your inventory.");
+        
             return Tuple.Create(currentRoom, inventory);
         }
 
-        public static Tuple<Room, List<Item>> RemoveFromInventory(Room currentRoom, string item, List<Item> inventory)
+        public static Tuple<Room, List<Item>> RemoveFromInventory(Room currentRoom, Item item, List<Item> inventory)
         {
-            IEnumerable<Item> itemCheck = inventory.Where(p => p.Name == item);
-            if (!itemCheck.Any())
-            {
-                Console.WriteLine("That is not currently in your inventory.");
-            }
-            else
-            {
-                Item removeItem = itemCheck.Single();
-                inventory.Remove(removeItem);
-                currentRoom.Inventory.Add(removeItem);
-                Console.WriteLine();
-                Console.WriteLine(item + " has been removed from your inventory.");
-            }
+            inventory.Remove(item);
+            currentRoom.Inventory.Add(item);
+            Console.WriteLine();
+            Console.WriteLine(item.Name + " has been removed from your inventory.");
+
             return Tuple.Create(currentRoom, inventory);
         }
     }
--- BlankGame/Program.cs
+++ BlankGame/Program.cs
@@ -50,14 +50,12 @@ namespace BlankGame
             Console.WriteLine("T) Test Room (Cave Room 5)");
             Console.WriteLine("Note: When in doubt, Help");
             string result = Console.ReadLine();
-            switch (result)
+            switch (result.ToLower())
             {
                 case "1":
                     return "Town Square";
                 case "t":
-                case "T":
                     return "Cave Room 5";
-                case "X":
                 case "x":
                     Console.WriteLine("Thanks for playing!!!");
                     return "Exit";
@@ -78,45 +76,77 @@ namespace BlankGame
             // Get player action and process
             Console.Write("Action: ");
             string result = Console.ReadLine();
+            result = result.ToLower();
 
             // **Dynamic Actions**
             //
             // Pickup Item action
             // If valid item, move item from room inventory to player inventory
-            if (result.Contains("Pickup") || result.Contains("pickup"))
+            if (result.Contains("pickup"))
             {
                 string pickedItem = result.Remove(0, 7);
-                Tuple <Room, List<Item>> updatedRoomInventory = ActionsInventory.AddToInventory(room, pickedItem, currentInventory);
-                gameAreas.Remove(room);
-                gameAreas.Add(updatedRoomInventory.Item1);
-                currentInventory = updatedRoomInventory.Item2;
+                IEnumerable<Item> checkValidItem = room.Inventory.Where(p => p.Name.ToLower() == pickedItem);
+                if (checkValidItem.Count() == 1)
+                {
+                    Item selectedItem = checkValidItem.Single();
+                    Tuple<Room, List<Item>> updatedRoomInventory = ActionsInventory.AddToInventory(room, selectedItem, currentInventory);
+                    gameAreas.Remove(room);
+                    gameAreas.Add(updatedRoomInventory.Item1);
+                    currentInventory = updatedRoomInventory.Item2;
+
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("That item does not exist here!");
+                }
+
 
                 return Tuple.Create(gameAreas, room.Name, currentInventory);
             }
 
             // Drop Item action
             // If item is in player inventory, move it to current room inventory
-            else if (result.Contains("Drop") || result.Contains("drop"))
+            else if (result.Contains("drop"))
             {
                 string removeItem = result.Remove(0, 5);
-                Tuple <Room, List<Item>> updatedRoomInventory = ActionsInventory.RemoveFromInventory(room, removeItem, currentInventory);
-                gameAreas.Remove(room);
-                gameAreas.Add(updatedRoomInventory.Item1);
-                currentInventory = updatedRoomInventory.Item2;
+                IEnumerable<Item> checkValidItem = currentInventory.Where(p => p.Name.ToLower() == removeItem);
+                if (checkValidItem.Count() == 1)
+                {
+                    Item selectedItem = checkValidItem.Single();
+                    Tuple<Room, List<Item>> updatedRoomInventory = ActionsInventory.RemoveFromInventory(room, selectedItem, currentInventory);
+                    gameAreas.Remove(room);
+                    gameAreas.Add(updatedRoomInventory.Item1);
+                    currentInventory = updatedRoomInventory.Item2;
+                }
+                else
+                {
+                    Console.WriteLine("That is not currently in your inventory.");
+                }
 
                 return Tuple.Create(gameAreas, room.Name, currentInventory);
             }
 
             // Look at Item action
-            else if (result.Contains("Look at") || result.Contains("look at") || result.Contains("look At") || result.Contains("Look At"))
+            else if (result.Contains("look at"))
             {
                 string inspectItem = result.Remove(0, 8);
-                ActionsInventory.LookAtItem(inspectItem);
+                IEnumerable<Item> checkValidItem = currentInventory.Where(p => p.Name.ToLower() == inspectItem);
+                if (checkValidItem.Count() == 1)
+                {
+                    Item selectedItem = checkValidItem.Single();
+                    ActionsInventory.DisplayItemStats(selectedItem);
+                } else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("That item is not in your inventory");
+                }
+                
                 return Tuple.Create(gameAreas, room.Name, currentInventory);
             }
 
             // Move Item action
-            else if (result.Contains("Move") || result.Contains("move"))
+            else if (result.Contains("move"))
             {
                 string objectToMove = result.Remove(0, 5);
                 gameAreas = Actions.MoveObject(gameAreas, room.Name, objectToMove);
@@ -133,13 +163,11 @@ namespace BlankGame
                 {
                     // Display Help Information
                     case "help":
-                    case "Help":
                         Actions.Help();
                         return Tuple.Create(gameAreas, room.Name, currentInventory);
                     
                     // Display current room information
                     case "look":
-                    case "Look":
                         if (room.Name.Contains("Cave"))
                         {
                             Actions.LookCave(room, currentInventory);
@@ -152,9 +180,6 @@ namespace BlankGame
 
                     // Travel North
                     case "go north":
-                    case "Go north":
-                    case "go North":
-                    case "Go North":
                         {
                             string nextRoom = Actions.Travel(room.Name, room.toNorth);
                             return Tuple.Create(gameAreas, nextRoom, currentInventory);
@@ -162,9 +187,6 @@ namespace BlankGame
 
                     // Travel South
                     case "go south":
-                    case "Go south":
-                    case "go South":
-                    case "Go South":
                         {
                             string nextRoom = Actions.Travel(room.Name, room.toSouth);
                             return Tuple.Create(gameAreas, nextRoom, currentInventory);
@@ -172,9 +194,6 @@ namespace BlankGame
 
                     // Travel East
                     case "go east":
-                    case "Go east":
-                    case "go East":
-                    case "Go East":
                         {
                             string nextRoom = Actions.Travel(room.Name, room.toEast);
                             return Tuple.Create(gameAreas, nextRoom, currentInventory);
@@ -182,9 +201,6 @@ namespace BlankGame
 
                     // Travel West
                     case "go west":
-                    case "Go west":
-                    case "go West":
-                    case "Go West":
                         {
                             string nextRoom = Actions.Travel(room.Name, room.toWest);
                             return Tuple.Create(gameAreas, nextRoom, currentInventory);
@@ -192,9 +208,6 @@ namespace BlankGame
 
                     // Travel to the Cave Dungeon
                     case "enter cave":
-                    case "Enter cave":
-                    case "enter Cave":
-                    case "Enter Cave":
                         {
                             if (room.Name == "Forest")
                             {
@@ -210,21 +223,16 @@ namespace BlankGame
 
                     // Display Player Inventory
                     case "show inventory":
-                    case "Show inventory":
-                    case "show Inventory":
-                    case "Show Inventory":
                         {
                             ActionsInventory.DisplayInventory(currentInventory);
                             return Tuple.Create(gameAreas, room.Name, currentInventory);
                         }
 
                     // Exit to Main Menu
-                    case "Exit":
                     case "exit":
                         return Tuple.Create(gameAreas, "MainMenu", currentInventory);
 
                     // Clear Screen
-                    case "Clear":
                     case "clear":
                         Console.Clear();
                         return Tuple.Create(gameAreas, room.Name, currentInventory);
