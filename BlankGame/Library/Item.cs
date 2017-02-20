97071414ccf527af3f278fec99decf1474f15efa
Christopher Upshir <christopherupshir@ccvonline.com>
2017-02-20 00:02:06 -0700

    Renamed Library

--- BlankGame/Library/Item.cs
+++ BlankGame/Library/Item.cs
@@ -65,10 +65,10 @@ namespace BlankGame
             return items;
         }
 
+        // Add item to Room Inventory
         public static Item AddItemToRoomInventory(string item)
         {
             Item addItem = new Item();
-            List<Item> roomItems = new List<Item>();
             if (item != "")
             {
                 List<Item> validItems = Item.ValidItems();
@@ -84,6 +84,63 @@ namespace BlankGame
                 
             }
             return addItem;
+        }
+
+        public static void DisplayInventory(List<Item> inventory)
+        {
+            Console.Clear();
+            Console.WriteLine();
+            UI.DisplayCenterText("Current Inventory");
+            UI.DisplayCenterText("---------------------------------------------------");
+            Console.WriteLine();
+            if (!inventory.Any())
+            {
+                UI.DisplayCenterText("<--Inventory Empty-->");
+            }
+            else
+            {
+                foreach (Item item in inventory)
+                {
+                    UI.DisplayCenterText(item.Name);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public static void DisplayItemStats(Item item)
+        {
+            Console.Clear();
+            UI.DisplayCenterText(item.Name + " Details");
+            UI.DisplayCenterText("---------------------------------------------------");
+            Console.WriteLine();
+            UI.DisplayCenterText("       Level: " + item.Level);
+            UI.DisplayCenterText("    Strength: " + item.Strength);
+            UI.DisplayCenterText("     Stamina: " + item.Stamina);
+            UI.DisplayCenterText("     Agility: " + item.Agility);
+            UI.DisplayCenterText("Intelligence: " + item.Intelligence);
+            Console.WriteLine();
+        }
+
+        public static Tuple<Room, List<Item>> AddToInventory(Room currentRoom, Item item, List<Item> inventory)
+        {
+            inventory.Add(item);
+            currentRoom.Inventory.Remove(item);
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine(item.Name + " has been added to your inventory.");
+
+            return Tuple.Create(currentRoom, inventory);
+        }
+
+        public static Tuple<Room, List<Item>> RemoveFromInventory(Room currentRoom, Item item, List<Item> inventory)
+        {
+            inventory.Remove(item);
+            currentRoom.Inventory.Add(item);
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine(item.Name + " has been removed from your inventory.");
+
+            return Tuple.Create(currentRoom, inventory);
         }
 
     }
